6c22b5f73ecaa51358734c6ebe4d57b9563d0479
MrEpoch <alexandrstencuk@seznam.cz>
2023-03-24 17:08:10 +0100

    I think it looks now with new colors more perfect, i will post it to github soon with my photo and some more real info

--- src/components/main-body.jsx
+++ src/components/main-body.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./styles/main-body.css"
-import contactImg from "./images/contact.png"
 
 export default function Main() {
     return (    
@@ -10,6 +9,78 @@ export default function Main() {
                     <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
                     <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
                     <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <div class="snow">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1536" preserveAspectRatio="xMidYMax slice">
+                    <g fill="#051622" fill-opacity=".15" transform="translate(55 42)">
+                    <g id="snow-bottom-layer">
+                        <ellipse cx="6" cy="1009.5" rx="6" ry="5.5"/>
+                        <ellipse cx="138" cy="1110.5" rx="6" ry="5.5"/>
+                        <ellipse cx="398" cy="1055.5" rx="6" ry="5.5"/>
+                        <ellipse cx="719" cy="1284.5" rx="6" ry="5.5"/>
+                        <ellipse cx="760" cy="1155.5" rx="6" ry="5.5"/>
+                        <ellipse cx="635" cy="1459.5" rx="6" ry="5.5"/>
+                        <ellipse cx="478" cy="1335.5" rx="6" ry="5.5"/>
+                        <ellipse cx="322" cy="1414.5" rx="6" ry="5.5"/>
+                        <ellipse cx="247" cy="1234.5" rx="6" ry="5.5"/>
+                        <ellipse cx="154" cy="1425.5" rx="6" ry="5.5"/>
+                        <ellipse cx="731" cy="773.5" rx="6" ry="5.5"/>
+                        <ellipse cx="599" cy="874.5" rx="6" ry="5.5"/>
+                        <ellipse cx="339" cy="819.5" rx="6" ry="5.5"/>
+                        <ellipse cx="239" cy="1004.5" rx="6" ry="5.5"/>
+                        <ellipse cx="113" cy="863.5" rx="6" ry="5.5"/>
+                        <ellipse cx="102" cy="1223.5" rx="6" ry="5.5"/>
+                        <ellipse cx="395" cy="1155.5" rx="6" ry="5.5"/>
+                        <ellipse cx="826" cy="943.5" rx="6" ry="5.5"/>
+                        <ellipse cx="626" cy="1054.5" rx="6" ry="5.5"/>
+                        <ellipse cx="887" cy="1366.5" rx="6" ry="5.5"/>
+                        <ellipse cx="6" cy="241.5" rx="6" ry="5.5"/>
+                        <ellipse cx="138" cy="342.5" rx="6" ry="5.5"/>
+                        <ellipse cx="398" cy="287.5" rx="6" ry="5.5"/>
+                        <ellipse cx="719" cy="516.5" rx="6" ry="5.5"/>
+                        <ellipse cx="760" cy="387.5" rx="6" ry="5.5"/>
+                        <ellipse cx="635" cy="691.5" rx="6" ry="5.5"/>
+                        <ellipse cx="478" cy="567.5" rx="6" ry="5.5"/>
+                        <ellipse cx="322" cy="646.5" rx="6" ry="5.5"/>
+                        <ellipse cx="247" cy="466.5" rx="6" ry="5.5"/>
+                        <ellipse cx="154" cy="657.5" rx="6" ry="5.5"/>
+                        <ellipse cx="731" cy="5.5" rx="6" ry="5.5"/>
+                        <ellipse cx="599" cy="106.5" rx="6" ry="5.5"/>
+                        <ellipse cx="339" cy="51.5" rx="6" ry="5.5"/>
+                        <ellipse cx="239" cy="236.5" rx="6" ry="5.5"/>
+                        <ellipse cx="113" cy="95.5" rx="6" ry="5.5"/>
+                        <ellipse cx="102" cy="455.5" rx="6" ry="5.5"/>
+                        <ellipse cx="395" cy="387.5" rx="6" ry="5.5"/>
+                        <ellipse cx="826" cy="175.5" rx="6" ry="5.5"/>
+                        <ellipse cx="626" cy="286.5" rx="6" ry="5.5"/>
+                        <ellipse cx="887" cy="598.5" rx="6" ry="5.5"/>
+                    </g>
+                    </g>
+                    <g fill="#051622" fill-opacity=".3" transform="translate(65 63)">
+                    <g id="snow-top-layer">
+                        <circle cx="8" cy="776" r="8"/>
+                        <circle cx="189" cy="925" r="8"/>
+                        <circle cx="548" cy="844" r="8"/>
+                        <circle cx="685" cy="1115" r="8"/>
+                        <circle cx="858" cy="909" r="8"/>
+                        <circle cx="874" cy="1438" r="8" transform="rotate(180 874 1438)"/>
+                        <circle cx="657" cy="1256" r="8" transform="rotate(180 657 1256)"/>
+                        <circle cx="443" cy="1372" r="8" transform="rotate(180 443 1372)"/>
+                        <circle cx="339" cy="1107" r="8" transform="rotate(180 339 1107)"/>
+                        <circle cx="24" cy="1305" r="8" transform="rotate(180 24 1305)"/>
+                        <circle cx="8" cy="8" r="8"/>
+                        <circle cx="189" cy="157" r="8"/>
+                        <circle cx="548" cy="76" r="8"/>
+                        <circle cx="685" cy="347" r="8"/>
+                        <circle cx="858" cy="141" r="8"/>
+                        <circle cx="874" cy="670" r="8" transform="rotate(180 874 670)"/>
+                        <circle cx="657" cy="488" r="8" transform="rotate(180 657 488)"/>
+                        <circle cx="443" cy="604" r="8" transform="rotate(180 443 604)"/>
+                        <circle cx="339" cy="339" r="8" transform="rotate(180 339 339)"/>
+                        <circle cx="24" cy="537" r="8" transform="rotate(180 24 537)"/>
+                    </g>
+                    </g>
                 </svg>
             </div>
             <div className="nothing"></div>
@@ -37,7 +108,6 @@ export default function Main() {
                   <h3>Email: noemail@noemail.no</h3>
                   <h3>Social Media: I don't know</h3>
               </div>
-              <img src={contactImg} className="contact-img" alt="" srcSet="" />
             </div>
         </main>
     )
